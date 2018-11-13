9ed97a5dcb646b286a485abbba48334157766a23
Administrator <admin@example.com>
2018-11-13 15:36:58 +0800

    log

--- src/main/java/test/UtilsMethodTest.java
+++ src/main/java/test/UtilsMethodTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -14,24 +16,30 @@ import utils.TimeUtils;
  */
 public class UtilsMethodTest {
 
+    private final static Logger logger = LoggerFactory.getLogger(UtilsMethodTest.class);
+
     @BeforeTest
     public void testTimeUtils() {
+        logger.info("before test timeutils");
         System.out.println(TimeUtils.getNowTime());
     }
 
     @Test
     public void testJDBCUtils() {
+        logger.info("test jdbcutils");
         System.out.println(JDBCUtils.getInstance("root", "", "jdbc:mysql://192.168.21.238:3306?useSSL=false&allowMultiQueries=true").query("select * from mysql.user"));
     }
 
     @Test
     public void testJsonUtils() {
+        logger.info("test jsonutils");
         System.out.println((JSONUtils.str2List("[1,2,3]")).get(0));
         Assert.assertEquals(1, 1);
     }
 
     @AfterTest
     public void testHttpUtils() {
+        logger.info("test httputils");
         System.out.println(HttpUtils.getRequest("https://www.baidu.com/s?wd=test"));
     }
 
