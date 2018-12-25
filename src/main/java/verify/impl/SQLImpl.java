cc80f18ad046fb2f0fc4b08625ef5b06b9f657a1
Administrator <admin@example.com>
2018-12-25 19:09:45 +0800

    sql log

--- src/main/java/verify/impl/SQLImpl.java
+++ src/main/java/verify/impl/SQLImpl.java
@@ -1,6 +1,8 @@
 package verify.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import utils.conn.impl.MysqlImpl;
 import verify.IVerify;
 import verify.modle.SQLModel;
@@ -13,17 +15,16 @@ import java.io.IOException;
 public class SQLImpl implements IVerify {
     private SQLModel sqlModel;
     private String result = "";
-
+    private static final Logger logger = LoggerFactory.getLogger(SQLImpl.class);
     @Override
     public void verify(String str) {
-        Object o = null;
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            o = objectMapper.readValue(str, SQLModel.class);
-            sqlModel = (SQLModel) o;
+            sqlModel = objectMapper.readValue(str, SQLModel.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        logger.info("Exec Sql: " + sqlModel.getSql());
         while (result.equals(sqlModel.getException())) {
             result = String.valueOf(MysqlImpl.getInstance(sqlModel.getUser(), sqlModel.getPw(), sqlModel.getUrl()).query(sqlModel.getSql()).get(0).get(sqlModel.getQuery()));
             try {
@@ -32,5 +33,6 @@ public class SQLImpl implements IVerify {
                 e.printStackTrace();
             }
         }
+        logger.info("Check Sql Result: " + sqlModel.getQuery() + "=" + sqlModel.getException() + "Success");
     }
 }
